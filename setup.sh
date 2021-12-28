78baabd56e494271d667cd1865124c7daa37c2d8
Kaiyu Zheng <kaiyutony@gmail.com>
2021-12-28 17:03:42 -0500

    adding the emotional speech dataset

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@ cd datasets/
 
 ## Simple tabular housing dataset
 if [ ! -d "bostonhousing" ]; then
+    echo -e "Downloading Boston Housing dataset"
     kaggle datasets download -d schirmerchad/bostonhoustingmlnd
     unzip bostonhoustingmlnd.zip -d bostonhousing
     rm bostonhoustingmlnd.zip
@@ -12,13 +13,23 @@ fi
 
 ## basics image -- mnist
 if [ ! -d "mnist" ]; then
+    echo -e "Downloading MNIST (csv) dataset (competition)"
     kaggle competitions download -c digit-recognizer
     unzip digit-recognizer.zip -d mnist
     rm digit-recognizer.zip
 fi
 
+## emotional speech dataset
+if [ ! -d "emotionalspeech" ]; then
+    echo -e "Downloading Emotional Speech dataset"
+    kaggle datasets download -d uwrfkaggler/ravdess-emotional-speech-audio
+    unzip ravdess-emotional-speech-audio.zip -d emotionalspeech
+    rm ravdess-emotional-speech-audio.zip
+fi
+
 ## stretch -- $160,000 Kaggle Competition; natural language processing
 if [ ! -d "feedbackprize" ]; then
+    echo -e "Downloading Feedback Prize dataset (competition)"
     kaggle competitions download -c feedback-prize-2021
     unzip feedback-prize-2021.zip -d feedbackprize
     rm feedback-prize-2021.zip
