6adfb6c647d564063e5d44a23d8dda4630e8ad07
Kaiyu Zheng <kaiyutony@gmail.com>
2022-05-05 17:15:04 -0400

    refactoring

--- zkynet/framework/cg.py
+++ zkynet/framework/cg.py
@@ -1,21 +1,8 @@
 """
-Components for the computation graph (a DAG):
-
-A node is either a Function or an Input.
-
-An Input must be at the end of any path on the
-graph. We can assign value to an Input.
-
-A Function takes in multiple inputs (i.e. nodes). It
-can be "called" to produce an output value, given
-that all inputs have assigned values.
-
-There is no special meaning for edges other
-than connectivity.
+A framework to define functions with corresponding,
+dynamically generated computational graph. Gradients
+are computed using automatic differentiation.
 """
-
-import numpy as np
-
 
 def _check_inputs_instantiated(inputs):
     return all(inputs[name].instantiated
@@ -27,72 +14,82 @@ def _get_uninstantiated_inputs(inputs):
             for name in inputs
             if not inputs[name].instantiated}
 
-
-def _check_input_types_valid(self, inputs):
-    return
-
-
-class Node:
-    """Node in the computation graph, a DAG"""
-    def __init__(self, children):
+########## Template objects ###########
+class Function:
+    """
+    A Function is an abstract template that maps
+    inputs (ordered) to an output subject to some
+    internal parameters.
+    """
+    def __init__(self, inputs, params={}):
         """
         Args:
-            children (dict): mapping from name to Node
-            parents (dict): mapping from name to Node
-        """
-        self._children = children
-
-    @property
-    def isleaf(self):
-        return len(self._children) == 0
-
-
-class Function(Node):
-    def __init__(self, inputs=tuple(), params={}):
-        """
-        A Function takes inputs and produces an output. The output
-        (of type Value) is only defined when all input variables are assigned.
-
-        The function can be parameterized.  These parameters can either
-        be constant (of type Constant) or changeable (of type Parameter).
-
-        Internally, both inputs (Variables) and Parameters (or Constants) are
-        Input nodes on the computational graph.
-
-        The inputs (non-parameters) are ordered.
-
-        Mathematically, the function is:
-
-            f (inputs ; params)
-
-        Args:
-            inputs (tuple): a tuple of ordered inputs, each
-                a Variable or a Function object.
+            inputs (tuple): a tuple of ordered inputs, each a Variable.
             params (dict): maps from parameter name to a Parameter
                 or a Constant.
         """
         assert all(isinstance(inputs[name], Variable) for name in inputs),\
-            f"inputs must be Variable or Function. "
+            f"all objects in 'inputs' must be of type Variable"
+        assert all(isinstance(params[name], Parameter)\
+                   or isinstance(params[name], Constant) for name in params),\
+            f"all objects in 'params' must be of type Parameter or Constant"
 
         self._ordered_input_names = (inp.name for inp in inputs)
-        children = {**{inp.name: inp for inp in inputs}, **params}
-        super().__init__(children)
+        self._inputs = {inp.name: inp for inp in inputs}
+        self._params = params
 
     def param(self, name):
         if name not in self._params:
             raise ValueError(f"{name} is not a parameter.")
-        return self.inputs[name]
+        return self._params[name]
 
     @property
     def inputs(self):
-        return self._children
+        return self._inputs
 
-    def call(self, **inputs):
+    def call(self, *inputs, **call_args):
         """Function to be overriden"""
         raise NotImplementedError
 
-    def _assign_inputs(self, inputs):
-        """inputs (dict)"""
+    def __call__(self, *input_vals, **call_args):
+        """The function is called (forward-pass).
+
+        At call time, the inputs are assigned variables.
+
+        Args:
+            *input_vals: each input is the value of an input
+                that defines this function. Order matters.
+                This value is either just a value (e.g. numpy array),
+                an InputNode, or a ValueNode. In the first case,
+                a corresponding InputNode will be generated and
+                assigned the given value.
+            *input_vals: each input is the value of an input
+                that defines this function. Order matters.
+            **call_args: call-time configurations to pass down
+                 to call.
+
+        Returns:
+            Value: an object that represents a node in the grounded
+                computational graph.
+        """
+        assigned_inputs = self._assign_inputs(*input_vals)
+        if not _check_inputs_instantiated(assigned_inputs):
+            raise ValueError("When calling a function, all its inputs must be instantiated.\n"\
+                             "The uninstantiated inputs are:\n",
+                             _get_uninstantiated_inputs(assigned_inputs))
+        output = self.call(*assigned_inputs, **call_args)
+        # For most cases, the user when implementing 'call' won't
+        # be bothered by the internal workings of this computation
+        # graph. Therefore, we will wrap the user's output with Value.
+        if not isinstance(output, Value):
+            return Value(assigned_inputs, output)
+        else:
+            raise ValueError(f"Output of {type(self)}.call is of type Value."\
+                             "This is unexpected. Try returning the output"\
+                             "without constructing the Value object.")
+
+    def _assign_inputs(self, *inputs):
+        """inputs:"""
         for varname in self._vars:
             if varname not in inputs:
                 raise ValueError(f"Variable {varname} is not assigned.")
@@ -104,78 +101,20 @@ class Function(Node):
                 raise ValueError("Constants cannot be assigned.")
             self.inputs[name].assign(inputs[varname])
 
-    def __call__(self, *inputs, **kwargs):
-        """The function is called (forward-pass)
 
-        Args:
-            *inputs: the values to inputs in the order that
-                 defines this function. The inputs to to this function
-                 will be assigned to the given value. Note that
-                (1) only Variables and Parameters can be assigned (the latter should
-                be optional); (2) the function throws an exception
-                if not all Variables are assigned a value.
-
-            **kwargs: other assignments, perhaps
-
-        Returns:
-            Value: an object that represents a node in the grounded
-                computational graph.
-        """
-        if not _check_inputs_instantiated(inputs):
-            raise ValueError("When calling a function, its inputs must be instantiated.\n"\
-                             "The uninstantiated inputs are:\n",
-                             _get_uninstantiated_inputs(inputs))
-        self._assign_inputs(inputs)
-        output = self.call(**inputs)
-        # For most cases, the user when implementing 'call' won't
-        # be bothered by the internal workings of this computation
-        # graph. Therefore, we will wrap the user's output with Value.
-        if isinstance(output, Value):
-            print(f"Warning: output of {type(self)}.call is of type Value."\
-                  "It will be wrapped by another Value.")
-        return Value(inputs, output)
-
-    def _build_grad_fn(self):
-        pass
-
-    def grad(self, inpt):
-        """Returns a dictionary that maps function that can be called to compute
-        the gradient to this function"""
-        raise NotImplementedError
-
-
-class Input(Node):
-    """An Input is a leaf node on the DAG"""
-    def __init__(self, input_type):
-        super().__init__({})
+class Input:
+    """An Input is an abstract template
+    for an input to a function, but without
+    a value."""
+    def __init__(self, name, input_type):
+        self.name = name
         self.input_type = input_type
-        self._value = None
-        self._grad = None  # stores the gradient
-
-    def assign(self, v):
-        self._value = v
-
-    @property
-    def value(self):
-        return self._value
-
-    def __str__(self):
-        return "{}({})".format(self.__class__.__name__, self.value)
-
-    def __repr__(self):
-        return str(self)
-
-    @property
-    def instantiated(self):
-        """Returns true if the variable is instantiated."""
-        return self._value is not None
 
 
 class Variable(Input):
     """Input variable; you have no control over."""
     def __init__(self, name):
         super().__init__(name, "variable")
-        self.name = name
 
 
 class Parameter(Input):
@@ -206,27 +145,54 @@ class Constant(Input):
         raise ValueError("Constant value cannot change")
 
 
-class Value(Node):
-    """Think about it as a function but after it is called, so
-    it is "grounded" to the input values. The inputs given
-    will be all converted to constants (separate from the
-    abstract computation graph).
+########### The computation graph components ##########
+class Node:
+    """Node in the computation graph, a DAG.
+    A node can always be regarded as an instantiation of
+    a particular Input to a Function. It carries a value.
 
-    Note: Value is immutable, and it is NOT designed to
-    be an input to a Function."""
-    def __init__(self, inputs, val, fun):
+    We distinguish two node types: InputNode and FunctionNode.
+    Don't confuse InputNode with Input; The InputNode
+    literally refers to a leaf node on the DAG, while Input
+    is just a placeholder of input in a Function template.
+
+    The InputNode is a leaf node, while the FunctionNode
+    is not a leaf node. Both should be grounded with values.
+    The value of the FunctionNode represents the output
+    of the function under some InputNode instantiation."""
+    def __init__(self, value, children={}, parent=None, parent_input_name=None):
         """
         Args:
-           inputs (dict-like): maps from name to Input; should be instantiated.
-           val (array-like): the actual value
-           fun (Function): the Function this Value grounds for.
+            children (dict): maps from string name to Node
+            parent (FunctionNode): the node of the function that
+                this node is an input for.
+            parent_input_name (str): the name of the input to the
+                parent function that this node corresponds to.
         """
-        assert _check_inputs_instantiated(inputs),\
-            "The inputs to a Value node must have been instantiated."
+        self._children = children
+        self._parent = parent
+        self._parent_input_name = parent_input_name
 
-        # convert inputs to constants, since they are no longer changeable.
-        _inputs = {name: Constant(inputs[name].value)}
+    def isleaf(self):
+        return len(self._children) == 0
 
-        super().__init__(_inputs)
-        self._value = val
+
+class InputNode(Node):
+    """A leaf node in the computational graph"""
+    def __init__(self, value, parent, parent_input_name):
+        super().__init__(value, parent=parent,
+                         parent_input_name=paren_input_name)
+
+
+class FunctionNode(Node):
+    """A non-leaf node in the computational graph"""
+    def __init__(self, fun, value, children, parent=None, parent_input_name=None):
+        """
+        Args:
+            fun (Function): the Function this node subsumes.
+        """
         self._fun = fun
+        super().__init__(value,
+                         children=children,
+                         parent=parent,
+                         parent_input_name=paren_input_name)
