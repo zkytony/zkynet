98156000e25380a0e28bfa3b095508ca32c06915
Kaiyu Zheng <kaiyutony@gmail.com>
2022-05-05 09:29:48 -0400

    wrote a test for the desirable autodiff lib behavior

--- tests/test_framework.py
+++ tests/test_framework.py
@@ -21,8 +21,24 @@ class MyTestModel1(Function):
 
 def test_model1():
     m = MyTestModel1()
-    print(m.params)   # expect to print out 'w'
-    x = np.array([3])
-    # We have now taken a forward-pass on m.
-    # it should accumulate gradients.
-    assert m(x) == (3+1)*3**2
+    assert m.params["w"] == 1  # initial value
+
+    # forward pass; constructs computation graph,
+    # and stores gradients.
+    result = m(x=np.array([3]))
+    assert result == (3+1)*3**2
+
+    # obtain the gradient of the function output with
+    # respect to inputs (these are numbers that are
+    # basically gradient function with assigned inputs
+    # and parameters).
+    dmdw = m.grad("w"); assert dmdw == 3**2
+    dmdx = m.grad("x"); assert dmdx == 3**2 + (3+1)*2*3
+
+    # obtain the gradient function; these are functions
+    # that you can later use. Note that they are also
+    # of type Function.
+    fun_dmdw = m.grad_fn("w")
+    fun_dmdx = m.grad_fn("x")
+    assert fun_dmdw(x=3, w=1) == dmdw
+    assert fun_dmdx(x=3, w=1) == dmdx
