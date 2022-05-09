3e9fdd9b90cd4f0e589e4ae773b090ecde01e68a
Kaiyu Zheng <kaiyutony@gmail.com>
2022-05-09 17:23:12 -0400

    util for getting full name space for an object

--- zkynet/utils.py
+++ zkynet/utils.py
@@ -2,3 +2,11 @@ import uuid
 
 def unique_id(length=6):
     return uuid.uuid4().hex[:length].upper()
+
+
+def fullname(o):
+    klass = o.__class__
+    module = klass.__module__
+    if module == 'builtins':
+        return klass.__qualname__ # avoid outputs like 'builtins.str'
+    return module + '.' + klass.__qualname__
