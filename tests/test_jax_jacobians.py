1a269750b520cd275205122b9562477040daab9b
Kaiyu Zheng <kaiyutony@gmail.com>
2022-05-17 00:55:55 -0400

    it looks like we found a way to correctly multiply two jacobian tensors

--- tests/test_jax_jacobians.py
+++ tests/test_jax_jacobians.py
@@ -7,20 +7,29 @@ description = "test jax jacobians"
 def f(z1, z2):
     return z1 * z2
 
-def z1(x):
-    return jnp.square(x)
+def z1(x, w):
+    return w*jnp.sin(jnp.square(x))
 
 def z2(x, w):
     return x + w
 
 def F(x, w):
-    _z1 = z1(x)
+    _z1 = z1(x,w)
     _z2 = z2(x,w)
     _f = f(_z1, _z2)
     return _f
 
+def dot(t1, t2):
+    # This is developed through trial and error;
+    if len(t1.shape) <= 2 and len(t2.shape) <= 2:
+        return jnp.dot(t1, t2)
+    elif len(t1.shape) <= 4 and len(t2.shape) <= 4:
+        return jnp.tensordot(t1, t2)
+    else:
+        return jnp.multiply(t1, t2)
+
 def test_forward_backward(x, w):
-    _z1 = z1(x)
+    _z1 = z1(x,w)
     _z2 = z2(x,w)
     _f = f(_z1, _z2)
 
@@ -34,12 +43,13 @@ def test_forward_backward(x, w):
     print("    dF/dz2 =\n", dFdz2)
 
     dz2dw = jacrev(z2, argnums=1)(x, w)
-    dFdw = jnp.dot(dFdz2, dz2dw)
+    dz1dw = jacrev(z1, argnums=1)(x, w)
+    dFdw = dot(dFdz1, dz1dw) + dot(dFdz2, dz2dw)
     print("    dF/dw  = dF/dz2 * dz2/dw =\n", dFdw)
     assert jnp.all(dFdw == jacrev(F, argnums=1)(x, w))
-    dz1dx = jacrev(z1, argnums=0)(x)
+    dz1dx = jacrev(z1, argnums=0)(x, w)
     dz2dx = jacrev(z2, argnums=0)(x, w)
-    dFdx = jnp.dot(dFdz1, dz1dx) + jnp.dot(dFdz2, dz2dx)
+    dFdx = dot(dFdz1, dz1dx) + dot(dFdz2, dz2dx)
     print("    dF/dx  = dF/dz1 * dz1/dx =\n", dFdx)
     assert jnp.all(dFdx == jacrev(F, argnums=0)(x, w))
 
@@ -64,17 +74,22 @@ def test_matrix_input():
 
 def test_tensor_input():
     print("============= test_tensor_input ==============")
-    x = jnp.array([[[3., 4., 5.],
-                    [-1., 2., -3.]],
-                   [[2., 0., 5.],
-                    [3., 1., -1.]]])
-    w = jnp.array(1.)
+    x = jnp.array([[[[3., 4., 5.]],
+                    [[-1., 2., -3.]]],
+                   [[[2., 0., 5.]],
+                    [[3., 1., -1.]]]])
+
+    # w = jnp.array(1.)
+    w = jnp.array([[[[1., 2., 1.]],
+                    [[1., 1., 1.]]],
+                   [[[-1., 1., 1.]],
+                    [[1., 1., 1.]]]])
     test_forward_backward(x, w)
 
 def run():
     test_scalar_input()
     test_vector_input()
-    test_matrix_input()
+    # test_matrix_input()
     test_tensor_input()
 
 if __name__ == "__main__":
