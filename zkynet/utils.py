bug fix - identity matrix multiplication in backpropdot

zkynet/utils.py
@@ -17,14 +17,12 @@ def backpropdot(t1, t2):
     """returns the product of two tensors t1 and t2
     that can deal with the case when t1 or t2 are
     vectors or matrices instead. Used in backprop."""
-    # This is developed through trial and error;
-    if len(t1.shape) <= 2 and len(t2.shape) <= 2:
+    if t1.shape == () and t1 == jnp.array(1.):
+        return t2
+    elif len(t1.shape) <= 2 and len(t2.shape) <= 2:
         return jnp.dot(t1, t2)
-    elif 0 < len(t1.shape) <= 4 and 0 < len(t2.shape) <= 4:
-        try:
-            return jnp.tensordot(t1, t2)
-        except:
-            return jnp.tensordot(t1, t2, axes=0)
+    elif len(t1.shape) <= 4 and len(t2.shape) <= 4:
+        return jnp.tensordot(t1, t2)
     else:
         # this seems to be the right call for tensor product
         # (though I am not sure why)